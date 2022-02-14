4789a807b2caa83a50165b4fe7e295c88a55c0b7
FernanValdes <fvaldes0109@gmail.com>
2022-02-13 20:53:17 -0500

    Optimizacion del Stemming
    See: #20

--- MoogleEngine/Stemming.cs
+++ MoogleEngine/Stemming.cs
@@ -1,3 +1,4 @@
+using System.Text;
 namespace MoogleEngine;
 
 // Clase destinada a la obtencion de la raiz de una palabra
@@ -11,8 +12,8 @@ public static class Stemming {
                                               "oslos", "oslas" };
 
     // Previos a los pronombres
-    static string[] prePronounA = new string[] { "iéndo", "ándo", "ár", "ér", "ír" };
-    static string[] prePronounB = new string[] { "iendo", "ando", "ar", "er", "ir", "an", "en", "e", "a" };
+    static string[] prePronounA = new string[] { "iéndo", "ándo", "ár", "ér", "ír", "án", "én", "é", "á", "ád" };
+    static string[] prePronounB = new string[] { "iendo", "ando", "ar", "er", "ir", "an", "en", "e", "a", "ad" };
     static string[] prePronounC = new string[] { "yendo" };
 
     // Sufijos estandares del paso 1
@@ -56,7 +57,7 @@ public static class Stemming {
     // Algoritmo: http://snowball.tartarus.org/algorithms/spanish/stemmer.html
     public static string GetRoot(string word) {
 
-        string result = word;
+        StringBuilder result = new StringBuilder(word);
 
         #region Calculando R1, R2 y RV
 
@@ -91,26 +92,63 @@ public static class Stemming {
 
         #endregion
 
+        #region Paso -1: Parche para adverbios terminados en 'te'
+
+        string stSufF = LongestSuffix(result, standSuffF);
+        if (stSufF != "" && result.Length - stSufF.Length >= R1) {
+            result.Remove(result.Length - stSufF.Length, stSufF.Length);
+            if (result.Length - 2 >= R1 && result[^2] == 'i' && result[^1] == 'v') { // R1 mio
+                result.Remove(result.Length - 2, 2);
+                if (result.Length - 2 >= R1 && result[^1] == 'd') { // R1 mio
+                    result.Remove(result.Length - 2, 2);
+                }
+            }
+            else {
+                string suff = LongestSuffix(result, standSuffF1);
+                if (suff != "" && result.Length - suff.Length >= R1) { // R1 mio
+                    result.Remove(result.Length - suff.Length, suff.Length);
+                }
+            }
+        }
+        else {
+            
+            string stSufG = LongestSuffix(result, standSuffG);
+            if (stSufG != "" && result.Length - stSufG.Length >= R1) { // R1 mio
+                result.Remove(result.Length - stSufG.Length, stSufG.Length);
+                string suff = LongestSuffix(result, standSuffG1);
+                if (suff != "" && result.Length - suff.Length >= RV) { // RV mio
+                    result.Remove(result.Length - suff.Length, suff.Length);
+                }
+            }
+        }
+
+        #endregion
+
         #region Paso 0: Pronombres
         
-        string suffix = LongestSuffix(word, pronouns);
-        string tillSuffix = word.Substring(0, word.Length - suffix.Length);
+        string suffix = LongestSuffix(result, pronouns);
+        string tillSuffix = result.ToString().Substring(0, result.Length - suffix.Length);
 
         string prevPron = "";
-        string prevA = LongestSuffix(tillSuffix, prePronounA);
-        string prevB = LongestSuffix(tillSuffix, prePronounB);
-        string prevC = LongestSuffix(tillSuffix, prePronounC);
 
         if (result.Length - suffix.Length >= R1) {
-            if (prevA != "") prevPron = prevA;
-            else if (prevB != "") prevPron = prevB;
-            else if (prevC != "" && tillSuffix.Length - prevC.Length >= 1 && tillSuffix[tillSuffix.Length - prevC.Length - 1] == 'u') {
-                prevPron = prevC;
+
+            string prevA = LongestSuffix(tillSuffix, prePronounA);
+            if (prevA != "" && tillSuffix.Length - prevA.Length >= RV) prevPron = prevA;
+            else {
+                string prevB = LongestSuffix(tillSuffix, prePronounB);
+                if (prevB != "" && tillSuffix.Length - prevB.Length >= RV) prevPron = prevB;
+                else {
+                    string prevC = LongestSuffix(tillSuffix, prePronounC);
+                    if (prevC != "" && tillSuffix.Length - prevC.Length >= RV && tillSuffix[tillSuffix.Length - prevC.Length - 1] == 'u') {
+                        prevPron = prevC;
+                    }
+                }
             }
 
             if (prevPron != "" && tillSuffix != "") {
-                result = tillSuffix;
-                if (prevA != "") result = StringParser.ParseAccents(result);
+                result.Remove(tillSuffix.Length, suffix.Length);
+                if (prevA != "") result = new StringBuilder(StringParser.ParseAccents(result.ToString()));
             }
         }
 
@@ -118,82 +156,80 @@ public static class Stemming {
 
         #region Paso 1: Sufijos estandar
 
-        string mark1 = result; // Para comprobar si se realizo el paso 1
+        string mark1 = result.ToString(); // Para comprobar si se realizo el paso 1
 
         string stSufA = LongestSuffix(result, standSuffA);
-        string stSufB = LongestSuffix(result, standSuffB);
-        string stSufC = LongestSuffix(result, standSuffC);
-        string stSufD = LongestSuffix(result, standSuffD);
-        string stSufE = LongestSuffix(result, standSuffE);
-        string stSufF = LongestSuffix(result, standSuffF);
-        string stSufG = LongestSuffix(result, standSuffG);
-        string stSufH = LongestSuffix(result, standSuffH);
-        string stSufI = LongestSuffix(result, standSuffI);
-        string stSufJ = LongestSuffix(result, standSuffJ);
-        string stSufK = LongestSuffix(result, standSuffK);
-
         if (stSufA != "" && result.Length - stSufA.Length >= R1) { // R1 modificacion mia
-            result = result.Substring(0, result.Length - stSufA.Length);
+            result.Remove(result.Length - stSufA.Length, stSufA.Length);
         }
-        else if (stSufB != "" && result.Length - stSufB.Length >= R1) { // R1 modificacion mia
-            result = result.Substring(0, result.Length - stSufB.Length);
-            if (result.EndsWith("ic") && result.Length - 2 >= R1) { // R1 modificacion mia
-                result = result.Substring(0, result.Length - 2);
-            }
-        }
-        else if (stSufC != "" && result.Length - stSufC.Length >= R1) { // R1 mio
-            result = result.Substring(0, result.Length - stSufC.Length);
-            result += "log";
-        }
-        else if (stSufD != "" && result.Length - stSufD.Length >= R1) { // R1 mio
-            result = result.Substring(0, result.Length - stSufD.Length);
-            result += "u";
-        }
-        else if (stSufE != "" && result.Length - stSufE.Length >= R1) { // R1 mio
-            result = result.Substring(0, result.Length - stSufE.Length);
-        }
-        else if (stSufF != "" && result.Length - stSufF.Length >= R1) {
-            result = result.Substring(0, result.Length - stSufF.Length);
-            if (result.EndsWith("iv") && result.Length - 2 >= R1) { // R1 mio
-                result = result.Substring(0, result.Length - 2);
-                if (result.EndsWith("at") && result.Length - 2 >= R1) { // R1 mio
-                    result = result.Substring(0, result.Length - 2);
+        else {
+
+            string stSufB = LongestSuffix(result, standSuffB);
+            if (stSufB != "" && result.Length - stSufB.Length >= R1) { // R1 modificacion mia
+                result.Remove(result.Length - stSufB.Length, stSufB.Length);
+                if (result.Length - 2 >= R1 && result[^2] == 'i' && result[^1] == 'c') { // R1 modificacion mia
+                    result.Remove(result.Length - 2, 2);
                 }
             }
             else {
-                string suff = LongestSuffix(result, standSuffF1);
-                if (suff != "" && result.Length - suff.Length >= R1) { // R1 mio
-                    result = result.Substring(0, result.Length - suff.Length);
+
+                string stSufC = LongestSuffix(result, standSuffC);
+                if (stSufC != "" && result.Length - stSufC.Length >= R1) { // R1 mio
+                    result.Remove(result.Length - stSufC.Length, stSufC.Length);
+                    result.Append("log");
+                }
+                else {
+
+                    string stSufD = LongestSuffix(result, standSuffD);
+                    if (stSufD != "" && result.Length - stSufD.Length >= R1) { // R1 mio
+                        result.Remove(result.Length - stSufD.Length, stSufD.Length);
+                        result.Append("u");
+                    }
+                    else {
+                        
+                        string stSufE = LongestSuffix(result, standSuffE);
+                        if (stSufE != "" && result.Length - stSufE.Length >= R1) { // R1 mio
+                            result.Remove(result.Length - stSufE.Length, stSufE.Length);
+                        }
+                        else {
+                            string stSufH = LongestSuffix(result, standSuffH);
+                            if (stSufH != "" && result.Length - stSufH.Length >= R1) { // R1 mio
+                                result.Remove(result.Length - stSufH.Length, stSufH.Length);
+                                string suff = LongestSuffix(result, standSuffH1);
+                                if (suff != "" && result.Length - suff.Length >= R1) { // R1 mio
+                                    result.Remove(result.Length - suff.Length, suff.Length);
+                                }
+                            }
+                            else {
+                                
+                                string stSufI = LongestSuffix(result, standSuffI);
+                                if (stSufI != "" && result.Length - stSufI.Length >= R2) {
+                                    result.Remove(result.Length - stSufI.Length, stSufI.Length);
+                                    if (result.Length - 2 >= R2 && result[^2] == 'a' && result[^1] == 't') {
+                                        result.Remove(result.Length - 2, 2);
+                                    }
+                                }
+                                else {
+                                    
+                                    string stSufJ = LongestSuffix(result, standSuffJ);
+                                    if (stSufJ != "" && result.Length - stSufJ.Length >= R1) {
+                                        if (result[result.Length - stSufJ.Length - 1] == 's') {
+                                            result.Remove(result.Length - stSufJ.Length, stSufJ.Length);
+                                        }
+                                    }
+                                    else {
+
+                                        string stSufK = LongestSuffix(result, standSuffK);
+                                        if (stSufK != "" && result.Length - stSufK.Length >= R1) {
+                                            result.Remove(result.Length - stSufK.Length, stSufK.Length);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        }
-        else if (stSufG != "" && result.Length - stSufG.Length >= R1) { // R1 mio
-            result = result.Substring(0, result.Length - stSufG.Length);
-            string suff = LongestSuffix(result, standSuffG1);
-            if (suff != "" && result.Length - suff.Length >= RV) { // RV mio
-                result = result.Substring(0, result.Length - suff.Length);
-            }
-        }
-        else if (stSufH != "" && result.Length - stSufH.Length >= R1) { // R1 mio
-            result = result.Substring(0, result.Length - stSufH.Length);
-            string suff = LongestSuffix(result, standSuffH1);
-            if (suff != "" && result.Length - suff.Length >= R1) { // R1 mio
-                result = result.Substring(0, result.Length - suff.Length);
-            }
-        }
-        else if (stSufI != "" && result.Length - stSufI.Length >= R2) {
-            result = result.Substring(0, result.Length - stSufI.Length);
-            if (result.EndsWith("at") && result.Length - 2 >= R2) {
-                result = result.Substring(0, result.Length - 2);
-            }
-        }
-        else if (stSufJ != "" && result.Length - stSufJ.Length >= R1) {
-            if (result[result.Length - stSufJ.Length - 1] == 's') {
-                result = result.Substring(0, result.Length - stSufJ.Length);
-            }
-        }
-        else if (stSufK != "" && result.Length - stSufK.Length >= R1) {
-            result = result.Substring(0, result.Length - stSufK.Length);
         }
 
         #endregion
@@ -201,30 +237,30 @@ public static class Stemming {
         #region Paso 2: Sufijos verbales
 
         // Si no se realizo el paso 1, realizar el 2
-        if (result == mark1) {
+        if (result.ToString() == mark1) {
 
-            string mark2a = result; // Para saber si se aplico el paso 2a
+            string mark2a = result.ToString(); // Para saber si se aplico el paso 2a
 
             string suffY = LongestSuffix(result, verbsY);
 
             if (suffY != "" && result.Length - suffY.Length >= RV && result.Length - suffY.Length >= 1 && result[result.Length - suffY.Length - 1] == 'u') {
-                result = result.Substring(0, result.Length - suffY.Length);
+                result.Remove(result.Length - suffY.Length, suffY.Length);
             }
 
             // Si no se realizo el paso 2a, realizar el 2b
-            if (mark2a == result) {
+            if (mark2a == result.ToString()) {
 
                 string suffB1 = LongestSuffix(result, verbsB1);
                 string suffB2 = LongestSuffix(result, verbsB2);
 
                 if (suffB1 != "" && result.Length - suffB1.Length >= RV && suffB2.Length < suffB1.Length) {
-                    result = result.Substring(0, result.Length - suffB1.Length);
-                    if (result.EndsWith("gu")) {
-                        result = result.Substring(0, result.Length - 1);
+                    result.Remove(result.Length - suffB1.Length, suffB1.Length);
+                    if (result[^2] == 'g' && result[^1] == 'u') {
+                        result.Remove(result.Length - 1, 1);
                     }
                 }
                 else if (suffB2 != "" && result.Length - suffB2.Length >= RV) {
-                    result = result.Substring(0, result.Length - suffB2.Length);
+                    result.Remove(result.Length - suffB2.Length, suffB2.Length);
                 }
             }
         }
@@ -236,18 +272,18 @@ public static class Stemming {
         string resSuf = LongestSuffix(result, residual);
 
         if (resSuf != "" && result.Length - resSuf.Length >= RV) {
-            result = result.Substring(0, result.Length - resSuf.Length);
+            result = result.Remove(result.Length - resSuf.Length, resSuf.Length);
         }
-        else if ((result.EndsWith('e') || result.EndsWith('é')) && result.Length - 1 >= RV) {
-            result = result.Substring(0, result.Length - 1);
-            if (result.EndsWith("gu") && result.Length - 1 >= RV) {
-                result = result.Substring(0, result.Length - 1);
+        else if ((result[^1] == 'e' || result[^1] == 'é') && result.Length - 1 >= RV) {
+            result.Remove(result.Length - 1, 1);
+            if (result.Length - 1 >= RV && result[^2] == 'g' && result[^1] == 'u') {
+                result.Remove(result.Length - 1, 1);
             }
         }
 
         #endregion
 
-        return StringParser.ParseAccents(result);
+        return StringParser.ParseAccents(result.ToString());
     }
 
     // Dada una palabra y una lista de sufijos, devuelve el mayor sufijo
@@ -257,6 +293,22 @@ public static class Stemming {
         string maxSuff = "";
         foreach (string suffix in suffixes) {
             if (suffix.Length > maxLen && word.EndsWith(suffix)) {
+                maxLen = suffix.Length;
+                maxSuff = suffix;
+            }
+        }
+        return maxSuff;
+    }
+
+    // Sobrecarga con StringBuilder
+    static string LongestSuffix(StringBuilder word, string[] suffixes) {
+        
+        string strWord = word.ToString();
+
+        int maxLen = 0;
+        string maxSuff = "";
+        foreach (string suffix in suffixes) {
+            if (suffix.Length > maxLen && strWord.EndsWith(suffix)) {
                 maxLen = suffix.Length;
                 maxSuff = suffix;
             }
